e35f3a54bb54d8f62cb3e0a9cb544ae42e33d645
yangzaorang <yangzaorang@didiglobal.com>
2020-08-06 21:30:14 +0800

    add demo

---
+++ demo/simple_crud/test_wt.c
@@ -0,0 +1,31 @@
+#include <wiredtiger.h>
+#include <wiredtiger_ext.h>
+
+int main() {
+    WT_CONNECTION *conn;
+    WT_CURSOR *cursor;
+    WT_SESSION *session;
+    const char *key, *value;
+    int ret;
+    /* Open a connection to the database, creating it if necessary. */
+    wiredtiger_open("./wt_meta", NULL, "create", &conn);
+    /* Open a session handle for the database. */
+    conn->open_session(conn, NULL, NULL, &session);
+    /* Create table. */
+    session->create(session, "table:aaa/my_table", "key_format=S,value_format=S");
+
+    session->open_cursor(session, "table:my_table", NULL, NULL, &cursor);
+    cursor->set_key(cursor, "key1");
+    cursor->set_value(cursor, "val1");
+    cursor->insert(cursor);
+
+    cursor->reset(cursor);
+
+    while((ret = cursor->next(cursor)) == 0) {
+        cursor->get_key(cursor, &key);
+        cursor->get_key(cursor, &value);
+        printf("Got record: %s : %s\n", key, value);
+    }
+
+    conn->close(conn, NULL);
+}
