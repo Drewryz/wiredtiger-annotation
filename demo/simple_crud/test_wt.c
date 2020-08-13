79f22f98bcb6b4af67081c0b5c9ba01dbb94e513
yangzaorang <yangzaorang@didiglobal.com>
2020-08-13 20:50:46 +0800

    update crud demo

--- demo/simple_crud/test_wt.c
+++ demo/simple_crud/test_wt.c
@@ -1,5 +1,27 @@
+/*
+ * @Author: yangzaorang 
+ * @Date: 2020-08-12 10:35:58 
+ * @Last Modified by: yangzaorang
+ * @Last Modified time: 2020-08-12 11:47:51
+ */
+
 #include <wiredtiger.h>
 #include <wiredtiger_ext.h>
+#include <stdio.h>
+
+void iterate_cursor(WT_CURSOR *cursor) {
+    int ret;
+    const char *key, *value;
+    int count = 0;
+    cursor->reset(cursor);
+    while((ret = cursor->next(cursor)) == 0) {
+        cursor->get_key(cursor, &key);
+        cursor->get_value(cursor, &value);
+        count++;
+        printf("record: %s : %s\n", key, value);
+    }
+    printf("------------------get %d record------------------\n", count);
+}
 
 int main() {
     WT_CONNECTION *conn;
@@ -12,20 +34,19 @@ int main() {
     /* Open a session handle for the database. */
     conn->open_session(conn, NULL, NULL, &session);
     /* Create table. */
-    session->create(session, "table:aaa/my_table", "key_format=S,value_format=S");
-
-    session->open_cursor(session, "table:aaa/my_table", NULL, NULL, &cursor);
+    session->create(session, "table:my_table", "key_format=S,value_format=S");
+    session->open_cursor(session, "table:my_table", NULL, NULL, &cursor);
+    printf("Insert a record.\n");
     cursor->set_key(cursor, "key1");
     cursor->set_value(cursor, "val1");
     cursor->insert(cursor);
-
+    printf("Inserting is success?\n");
+    iterate_cursor(cursor);
+    printf("Delete a record.\n");
     cursor->reset(cursor);
-
-    while((ret = cursor->next(cursor)) == 0) {
-        cursor->get_key(cursor, &key);
-        cursor->get_key(cursor, &value);
-        printf("Got record: %s : %s\n", key, value);
-    }
-
+    cursor->set_key(cursor, "key1");
+    cursor->remove(cursor);
+    printf("Deleting is success?\n");
+    iterate_cursor(cursor);
     conn->close(conn, NULL);
 }
